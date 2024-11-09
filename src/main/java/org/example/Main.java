c2def91c2c8478f6f7737d484b52920964cb457d
Amrit Kashyap <70964612+amrit-js@users.noreply.github.com>
2024-11-10 03:08:26 +0530

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        
+        Connection con = null;
+        try {
+            con = Database.getConnection();
+            if (con!=null){
+                System.out.println("Database Connected");
+            }
+            String Query1="INSERT INTO `test`.`users`(`id`,`password`,`amount`,`loan_amount`,`firstName`,`lastName`) VALUES (111,'pass',0,0,'test','test');";
+
+            con.createStatement();
+            Statement query = con.createStatement();
+            query.execute(Query1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
